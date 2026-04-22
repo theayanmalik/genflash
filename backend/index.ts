7010cd085c7b4ac8d1a890be4ec2a4452f4fe798
Md Ayan Parwez <87706546+theayanmalik@users.noreply.github.com>
2026-04-22 13:44:26 +0530

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -18,17 +18,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 // CORS
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "http://localhost:5173",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-)
+app.use(cors())
 
 app.use(express.json())
 
